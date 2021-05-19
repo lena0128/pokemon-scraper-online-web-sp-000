5a1dc7fbe6ef13ac3a5d8649e30f6618a50626d5
Lena Hou <lenahou2013@gmail.com>
2021-05-19 19:49:58 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,21 +9,15 @@ def initialize(id:nil, name:, type:, db:)
   @db = db
 end
 
-def self.save(name, type, db)
-  sql = <<-SQL
-    INSERT INTO pokemon (name, type)
-    VALUES (?, ?)
-  SQL
+def self.save (name, type, db)
+        sql = "INSERT INTO pokemon (name, type) VALUES (?, ?)"
+        db.execute(sql, name, type)
+        @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
 
-  @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
-end
-
-def self.find(id, db)
-    sql = <<-SQL
-      SELECT * FROM pokemon WHERE id = ?
-    SQL
-    pokemon = db.execute(sql, id)[0]
-    Pokemon.new(id:pokemon[0], name:pokemon[1], type:pokemon[2], db:db)
-  end
-
+    def self.find(id, db)
+        sql = "SELECT * FROM pokemon WHERE id = ?"
+        result = db.execute(sql, id)[0]
+        Pokemon.new(id:result[0], name:result[1], type:result[2], db: db)
+    end
 end

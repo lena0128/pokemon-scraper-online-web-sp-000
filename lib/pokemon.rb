6959b3fb448f1562b3517954adff736fd02a76dc
Lena Hou <lenahou2013@gmail.com>
2021-05-19 16:10:49 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,12 @@
 class Pokemon
-  attr_accessor :name, :type, :db
+  attr_accessor :name, :type, :hp, :db
   attr_reader :id
 
-  def initialize(id=nil, name, type, db)
+  def initialize(id=nil, name, type, hp, db)
     @id = id
     @name = name
     @type = type
+    @hp = hp
     @db = db
   end
 
